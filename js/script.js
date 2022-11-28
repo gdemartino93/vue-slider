48cfde46389d0e17c6005488267d8c3ce4011490
gdemartino93 <113302882+gdemartino93@users.noreply.github.com>
2022-11-28 18:10:24 +0100

    ok

--- js/script.js
+++ js/script.js
@@ -8,41 +8,52 @@ const { createApp } = Vue
                             image: 'img/01.webp',
                             title: 'Marvel\'s Spiderman Miles Morale',
                             text: 'Experience the rise of Miles Morales as the new hero masters incredible, explosive new powers to become his own Spider-Man.',
-                            id : 0
                         }, 
                         {
                             image: 'img/02.webp',
                             title: 'Ratchet & Clank: Rift Apart',
                             text: 'Go dimension-hopping with Ratchet and Clank as they take on an evil emperor from another reality.',
-                            id : 1 
                         },
                         {
                             image: 'img/03.webp',
                             title: 'Fortnite',
                             text: "Grab all of your friends and drop into Epic Games Fortnite, a massive 100 - player face - off that combines looting, crafting, shootouts and chaos.",
-                            id : 3
                         },
                         {
                             image: 'img/04.webp',
                             title: 'Stray',
                             text: 'Lost, injured and alone, a stray cat must untangle an ancient mystery to escape a long-forgotten city',
-                            id : 4
                         },
                         {
                             image: 'img/05.webp',
                             title: "Marvel's Avengers",
                             text: 'Marvel\'s Avengers is an epic, third-person, action-adventure game that combines an original, cinematic story with single-player and co-operative gameplay.',
-                            id : 5
                         }
                     ],
-                    attiva : 0,
+                    
                     dnone : "none",
-                    block : "block"
+                    block : "block",
+                    attiva : 0,
+                    message : "ciao ciao",
+                    timer : null
             }
         },
         methods:{
-            
+            giu(){
+                this.attiva--
+                if ( this.attiva < 0){
+                    this.attiva = this.slides.length - 1
+                }
+            },
+            su(){
+                this.attiva++
+            if ( this.attiva > this.slides.length - 1){
+                this.attiva = 0
             }
+            },   
+            
+         
+        }
         
 
     }).mount("#app")
